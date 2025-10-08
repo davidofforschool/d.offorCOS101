c0a24e5e29eaff77fa6cdc137a78e43916886e97
David Offor <davidoffor19@gmail.com>
2025-10-08 18:55:54 +0100

    practices only

---
+++ week-2/practices/practice_4.rs
@@ -0,0 +1,17 @@
+fn main(){
+    let p:f64 = 1000.0;
+    let r:f64 = 1.0;
+    let t:f64 = 2.0;
+
+    // for user clarification of the parameters, i can add something like:
+    /* println!("Principal is {}", p);
+       println!("Rate is {}", r);
+       println!("Time is {}", t);
+    */
+
+    // simple interest
+    let a = p * (1.0 + (r/100.0) * t);
+    println!("Amount is {}", a);
+    let si = a - p;
+    println!("Simple Interest is {}", si);
+}
