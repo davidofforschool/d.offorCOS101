1188c285f1af80982c5db6abb99d9e766e73cd96
David Offor <davidoffor19@gmail.com>
2025-10-11 21:57:37 +0100

    added projects folder properly

---
+++ week-2/projects/project_3.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let p:f64 = 510_000.00;
+    let r:f64 = 5.00;
+    let n:f64 = 3.00;
+
+    // compound interest calculation
+    let a = p * (1.0 - (r/100.0)).powf(n);
+    println!("Amount is {}", a);
+    let ci = p - a;
+    println!("Compound Interest is {}", ci);
+}
+
+
+git init
+git add .
+git commit -m "added project folder" 
+git push
