0c4243d80ce1c3be2a1a367df070557915969b2d
David Offor <davidoffor19@gmail.com>
2025-10-11 21:33:13 +0100

    added projects folder

--- week-2/practices/practice_4.rs
+++ week-2/practices/practice_4.rs
@@ -2,7 +2,7 @@ fn main(){
     let p:f64 = 1000.0;
     let r:f64 = 1.0;
     let t:f64 = 2.0;
-
+c
     // for user clarification of the parameters, i can add something like:
     /* println!("Principal is {}", p);
        println!("Rate is {}", r);
