4e9595505e87053eafeb149d7d3f1028bd7f59f3
David Offor <davidoffor19@gmail.com>
2025-10-22 13:14:11 +0100

    added practice 1 which requires user name and age input

---
+++ week-4/practice_1/target/debug/deps/practice_1-ca86158ad5c8e349.d
@@ -0,0 +1,5 @@
+/Users/davidoffor/Documents/d.offorCOS101/week-4/practice_1/target/debug/deps/practice_1-ca86158ad5c8e349.d: src/main.rs
+
+/Users/davidoffor/Documents/d.offorCOS101/week-4/practice_1/target/debug/deps/practice_1-ca86158ad5c8e349: src/main.rs
+
+src/main.rs:
---
+++ week-4/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+/Users/davidoffor/Documents/d.offorCOS101/week-4/practice_1/target/debug/practice_1: /Users/davidoffor/Documents/d.offorCOS101/week-4/practice_1/src/main.rs
