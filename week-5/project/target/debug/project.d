4aedb412fd9ccd3228e150b6cafea5e64cc90a86
davidofforschool <david.offor@pau.edu.ng>
2025-11-08 21:59:09 +0100

    changed folder name from week-5 to week-6 and completed the project

--- week-5/project/target/debug/project.d
+++
@@ -1,1 +0,0 @@
-/Users/davidoffor/Documents/d.offorCOS101/week-5/project/target/debug/project: /Users/davidoffor/Documents/d.offorCOS101/week-5/project/src/main.rs
---
+++ week-6/project/target/debug/project.d
@@ -0,0 +1,1 @@
+/Users/davidoffor/Documents/d.offorCOS101/week-6/project/target/debug/project: /Users/davidoffor/Documents/d.offorCOS101/week-6/project/src/main.rs
